0c5a87224a04522c1b502748e2f7c43b5df02df1
jeswsi <james@woodruffscientific.com>
2019-01-14 15:29:31 -0800

    changing schema

--- models/product.model.js
+++ models/product.model.js
@@ -3,8 +3,8 @@ var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
     user: {type: String, required: true, max: 100},
-    room: {type: Number, required: true}
-    data: {type: 'Buffer', required: true},
+    room: {type: Number, required: true},
+    data: {type: 'Buffer', required: true}
 });
 
 
