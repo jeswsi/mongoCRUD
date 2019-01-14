a1a39b7ccd2068fb9bc62e5b290705955b44d14c
jeswsi <james@woodruffscientific.com>
2019-01-14 15:19:41 -0800

    changed schema

--- models/product.model.js
+++ models/product.model.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var ProductSchema = new Schema({
-    name: {type: String, required: true, max: 100},
-    price: {type: Number, required: true},
+    user: {type: String, required: true, max: 100},
+    room: {type: Number, required: true}
+    data: {type: 'Buffer', required: true},
 });
 
 
